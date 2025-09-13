c8923df3b204e0a8755cbb0e2d40a111c06e3ce0
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2025-09-13 21:53:56 +0200

    fix(contact): text download cv ok

--- settings.js
+++ settings.js
@@ -152,6 +152,8 @@ about_years_value: "5+",
         contact_title_en: "Contact me",
         contact_intro_fr: "Disponible en région lilloise et alentours (véhiculé)",
         contact_intro_en: "Interested in collaboration? Feel free to contact me!",
+        contact_download_cv_fr: "Télécharger mon CV",
+        contact_download_cv_en: "Download my CV",
         
         // Footer
         footer_rights_fr: "Baptiste Lavogiez. Tous droits réservés.",
